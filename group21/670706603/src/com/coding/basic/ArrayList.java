25e2c0f56e6f6874ab7f7b346ab7a5aa317c7f3b
liyuhang521 <670706603@qq.com>
2017-02-26 13:56:49 +0800

    add
    add coding

---
+++ group21/670706603/src/com/coding/basic/ArrayList.java
@@ -0,0 +1,71 @@
+package com.coding.basic;
+
+import java.util.Arrays;
+
+public class ArrayList implements List {
+
+	private int size = 0;
+
+	private Object[] elementData = new Object[1];
+
+	/**
+	 * 数组顺序添加元素方法
+	 */
+	public void add(Object o) {
+		if (elementData.length > size) {
+			elementData[size] = o;
+			size++;
+		} else {
+			elementData = growarray(elementData);
+			elementData[size] = o;
+			size++;
+		}
+
+	}
+
+	/**
+	 * 数组指定位置插入元素方法
+	 */
+	public void add(int index, Object o) {
+
+	}
+
+	/**
+	 * 获取指定位置的元素
+	 */
+	public Object get(int index) {
+		if (index < elementData.length) {
+			return elementData[index];
+		} else {
+			return -1;
+		}
+	}
+
+	public Object remove(int index) {
+		if (index < elementData.length){
+			Object[] elementData1 = new Object[1];
+			elementData1=Arrays.copyOfRange(elementData, 0, index);
+			for(int i=index;i<elementData.length;i++){
+				elementData1.
+			}
+		}
+	}
+
+	public int size() {
+		return elementData.length;
+	}
+
+	public Iterator iterator() {
+		return null;
+	}
+
+	/**
+	 * 数组宽度增长方法
+	 */
+	private Object[] growarray(Object[] array) {
+		
+		return Arrays.copyOf(array, array.length+1);
+
+	}
+
+}
---
+++ group21/670706603/src/com/coding/basic/TestMethod.java
@@ -0,0 +1,10 @@
+package com.coding.basic;
+
+import java.util.ArrayList;
+
+public class TestMethod {
+public static void main(String[] args) {
+	ArrayList list=new java.util.ArrayList();
+	
+}
+}
